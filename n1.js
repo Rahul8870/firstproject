2ca13d9dd1f6aa9260e3c78a72f6c617177dcc7b
Rahul8870 <135112162+Rahul8870@users.noreply.github.com>
2023-06-06 16:36:08 +0530

    Update n1.js

--- n1.js
+++ n1.js
@@ -17,12 +17,6 @@ class emp {
   emp1.last_name = "ravi";
   emp1.id = "01";
   
-  emp1.dob = new Date(2001, 05, 15);
-  emp1.doj = new Date(2023, 04, 03);
-  emp1.qualification = "B-Tech";
-  emp1.skill = "Java";
-  let employee=[emp1];
-  console.log(emp1.employee_name());
-  console.log(emp1.employee_name());
+  
 
   
