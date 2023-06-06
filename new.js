c41358fcc2a90642197a30303a1e47c059506d5e
Rahul8870 <135112162+Rahul8870@users.noreply.github.com>
2023-06-06 16:47:43 +0530

    Update new.js

--- new.js
+++ new.js
@@ -16,11 +16,7 @@ class emp {
   emp1.first_name = "Rahul ";
   emp1.last_name = "ravi";
   emp1.id = "01";
-  
-  emp1.dob = new Date(2001, 05, 15);
-  emp1.doj = new Date(2023, 04, 03);
-  emp1.qualification = "B-Tech";
-  emp1.skill = "Java";
+
 
 
   
